758ebb0b1a8ea5d2d38909a6ac40238be157616d
leal <jorge.leal@cceo.com.mx>
2023-03-01 13:28:11 -0600

    Instalación y configuración Vue 3

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,15 @@
+<script>
+import List from './components/List.vue';
+export default {
+    components:{
+        List
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Principal</h1>
+        <List />
+    </div>
+</template>       
